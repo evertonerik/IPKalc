332cc49902313b0d9180eb71f0a3be6261b5f197
Everton Erik <everton_erik@hotmail.com>
2022-07-04 17:45:28 -0300

    Populate Table

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -23,12 +23,25 @@ void MainWindow::on_horizontalSlider_valueChanged(int value)
 {
     unsigned int mask = ( 4294967295  >> (32 - value) ) << (32 - value);
     QStringList ips = ui->lineEdit_IP->text().split(".");
-    unsigned int ip = (ips[0].toInt() << 24) | (ips[0].toInt() << 16) | (ips[0].toInt() << 8) | (ips[0].toInt() );
+    unsigned int ip = (ips[0].toInt() << 24) | (ips[1].toInt() << 16) | (ips[2].toInt() << 8) | (ips[3].toInt() );
     unsigned int network = ip&mask;
+    unsigned int wild = ~mask;
 
-    ui->tableWidget_Info->item(0,0)->setText(ui->lineEdit_IP->text());
-    //ui->tableWidget_Info->item(0,1)->setText(ip2Qstring(network));
+
     ui->lineEdit_CIDR->setText(QString::number(value));
     ui->lineEdit_Mask->setText(ip2Qstring(mask));
+
+    ui->tableWidget_Info->item(0,0)->setText(ui->lineEdit_IP->text());
+    ui->tableWidget_Info->item(1,0)->setText("Private");
+    ui->tableWidget_Info->item(2,0)->setText("C");
+    ui->tableWidget_Info->item(3,0)->setText(ip2Qstring(network));
+    ui->tableWidget_Info->item(4,0)->setText(ip2Qstring(network|wild));
+    ui->tableWidget_Info->item(5,0)->setText(ui->lineEdit_Mask->text());
+    ui->tableWidget_Info->item(6,0)->setText(ip2Qstring(wild));
+    ui->tableWidget_Info->item(7,0)->setText(QString::number(wild-1));
+    ui->tableWidget_Info->item(8,0)->setText(ip2Qstring(network+1));
+    ui->tableWidget_Info->item(9,0)->setText(ip2Qstring(network+1)+"-"+ip2Qstring((network|wild)-1));
+    ui->tableWidget_Info->item(10,0)->setText(ip2Qstring(network+2)+"-"+ip2Qstring((network|wild)-1));
+
 }
 
